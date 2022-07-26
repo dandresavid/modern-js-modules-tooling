8715ca3d9c7ad764b5290efa5fd72a6fad3722db
Andres David Blandon <dandresavid@hotmail.com>
2022-07-26 12:55:20 -0500

    Added package modules for npm libraries example

--- script.js
+++ script.js
@@ -10,12 +10,94 @@
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
 
-//Avoid this kind of mix, use mostly just default.
+//Avoid this kind of mix, use mostly just default and do not use object specific imports
 // import add, {addToCart, totalPrice as price, qt} from './shoppingCart.js';
 // console.log(price);
 
-import add,{cart} from './shoppingCart.js';
-add('Pizza', 2);
-add('bread', 5);
-add('apples', 4);
-console.log(cart)
+// import add,{cart} from './shoppingCart.js';
+// add('Pizza', 2);
+// add('bread', 5);
+// add('apples', 4);
+// console.log(cart);
+
+// await in this case is bloacking the execution
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+/*
+const getLastPost = async function() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+
+    return { title: data.at(-1).title, text: data.at(-1).body}
+}
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+*/
+
+/*
+const ShoppingCart2 = (function() {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice =237;
+    const totalQuantity = 23;
+
+    const addToCart = function(product, quantity) {
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} added to cart`)
+    };
+
+    const orderStock = function(product, quantity) {
+        console.log(`${quantity} ${product} ordered form supplier`)
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    }
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); //undefined
+*/
+
+// Export NodeJS
+// export.addToCart = function (product, quantity) {
+//     cart.push({product, quantity});
+//     console.log(`${quantity} ${product} added to cart`)
+// };
+
+//Import NodeJS
+// const {addToCart} = require('./shoppingCart.js')
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+    cart: [
+        { product: 'bread', quantity: 5},
+        { product: 'pizza', quantity: 2},
+    ],
+    user: {loggedIn: true},
+}
+const stateClone = Object.assign({},state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
+
